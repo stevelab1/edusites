d1c7f7af947b647afd43aad07ec5da9802f040b6
stevelab1 <stevetubeone@gmail.com>
2020-11-14 15:41:05 +0000

    temp

--- edusites/settings.py
+++ edusites/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
