9503afd3893d315861803a720ba273cd58eee54f
stevelab1 <stevetubeone@gmail.com>
2020-11-14 17:55:05 +0000

    model change image default

--- account/models.py
+++ account/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/',
-                              blank=True)
+                              blank=True, default='static/aftermath.jpg')
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
