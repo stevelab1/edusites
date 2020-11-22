16d60c706e5a6f83b71d19dfd5d238b4770fbdac
stevelab1 <stevetubeone@gmail.com>
2020-11-22 17:01:27 +0000

    asgi test

--- edusites/asgi.py
+++ edusites/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 import django
-from channels.routing import get_default_application
+from channels.routing *
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusites.settings')
