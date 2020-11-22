0a382c509cea16b0285e57d1d4c6a177f66653f4
stevelab1 <stevetubeone@gmail.com>
2020-11-22 14:40:13 +0000

    routing test import chat.consumers

--- edusites/asgi.py
+++ edusites/asgi.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 import django
 from channels.routing import get_default_application
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusites.settings')
 django.setup()
 application = get_default_application()
--- edusites/routing.py
+++ edusites/routing.py
@@ -1,6 +1,9 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 import chat.routing
+import chat.consumers
+
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(
