9ddc5a5769426e402582c44ab8212b0c241eb884
stevelab1 <stevetubeone@gmail.com>
2020-11-23 10:49:00 +0000

    asgi room.html tidy up

--- edusites/asgi.py
+++ edusites/asgi.py
@@ -7,13 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
-
 import os
 import django
-from channels.routing *
+from channels.routing import get_default_application
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusites.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 django.setup()
 application = get_default_application()
 
