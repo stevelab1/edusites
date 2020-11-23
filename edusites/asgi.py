5faef68f566d1dea62c38518d08d62695943ef63
stevelab1 <stevetubeone@gmail.com>
2020-11-23 11:01:09 +0000

    asgi tidy up

--- edusites/asgi.py
+++ edusites/asgi.py
@@ -11,7 +11,7 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edusites.settings')
 django.setup()
 application = get_default_application()
 
